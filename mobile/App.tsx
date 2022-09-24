014504d9aec0e374eb8b6e29158e0136cd79a026
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-09-24 22:12:15 +0100

    Load Linear Font to the project (Mobile Project)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,23 @@
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
 import { Background } from './src/components/Background';
 import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
       <StatusBar
@@ -11,7 +26,7 @@ export default function App() {
         translucent
       />
 
-      <Home />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 };
