86f32e13c1cdd44fd7cb40782cf5f57a29064ffe
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-09-30 19:04:08 +0100

    Customization of routes (Mobile Project)

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import { View, Image, FlatList } from 'react-native';
 import { useEffect, useState } from 'react';
+import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
 import { GameCard, GameCardProps } from '../../components/GameCard';
 import { styles } from './styles';
@@ -10,34 +11,36 @@ export function Home() {
 
   useEffect(() => {
     fetch('http://192.168.0.171:3333/games')
-    .then(response => response.json())
-    .then(data => setGames(data));
+      .then(response => response.json())
+      .then(data => setGames(data));
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={Logo}
-        style={styles.logo}
-      />
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={Logo}
+          style={styles.logo}
+        />
 
-      <Heading
-        title='Encontre seu duo!'
-        subtitle='Selecione o game que deseja jogar...'
-      />
+        <Heading
+          title='Encontre seu duo!'
+          subtitle='Selecione o game que deseja jogar...'
+        />
 
-      <FlatList
-        data={games}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard
-            data={item}
-          />
-        )}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-    </View>
+        <FlatList
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </View>
+    </Background>
   );
 };
