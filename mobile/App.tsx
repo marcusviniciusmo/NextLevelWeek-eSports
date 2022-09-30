99064a7fd2b640683d96c38dc7d512632533326e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-09-30 16:42:08 +0100

    Centralization of routes (Mobile Project)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import {
 } from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
 import { Background } from './src/components/Background';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 };
