6d13a8cfa052af1ceb9a15a63af10d6a5798bdf5
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-01 08:59:18 +0100

    Basic creation of the Game screen (Mobile Project)

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,11 @@
+import { Background } from '../../components/Background';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export function Game() {
+  return (
+    <Background>
+      <SafeAreaView>
+      </SafeAreaView>
+    </Background>
+  );
+};
