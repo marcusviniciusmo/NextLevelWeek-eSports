503955ca6e8939234eaaf6579d0ca29497133e03
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-02 08:11:00 +0100

    Inserting Entypo icon to go back (Mobile Project)

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,7 +1,10 @@
 import { Background } from '../../components/Background';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, TouchableOpacity } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { GameParams } from '../../@types/navigation';
+import { Entypo } from '@expo/vector-icons';
+import { THEME } from '../../theme';
 
 export function Game() {
   const route = useRoute();
@@ -10,6 +13,15 @@ export function Game() {
   return (
     <Background>
       <SafeAreaView>
+        <View>
+          <TouchableOpacity>
+            <Entypo
+              name='chevron-thin-left'
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </Background>
   );
