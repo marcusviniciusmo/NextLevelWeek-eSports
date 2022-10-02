ae00ab740b041d681c1a85aeb9fc061a7e2e072c
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-02 09:39:57 +0100

    DuoInfo component styling (Mobile Project)

--- mobile/src/components/DuoInfo/index.tsx
+++ mobile/src/components/DuoInfo/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text, ColorValue } from 'react-native';
 import { THEME } from '../../theme';
+import { styles } from './styles';
 
 interface DuoInfoProps {
   label: string;
@@ -9,9 +10,12 @@ interface DuoInfoProps {
 
 export function DuoInfo({ label, value, colorValue = THEME.COLORS.TEXT }: DuoInfoProps) {
   return (
-    <View>
-      <Text>{label}</Text>
-      <Text style={[{ color: colorValue }]}>
+    <View style={styles.container}>
+      <Text style={styles.label}>
+        {label}
+      </Text>
+
+      <Text style={[styles.value, { color: colorValue }]}>
         {value}
       </Text>
     </View>
