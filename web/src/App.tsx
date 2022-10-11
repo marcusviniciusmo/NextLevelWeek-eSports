96b43924c5c94a1f4ce03adf55129020e9eedb6a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-11 22:06:58 +0100

    Creation Games view (Web Project)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,52 +1,8 @@
-import { useEffect, useState } from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
-import LogoImage from './assets/logo-nlw-esports.svg';
-import CreateAdBanner from './components/CreateAdBanner';
-import GameBanner from './components/GameBanner';
-import './styles/main.css';
-import CreateAdModal from './components/CreateAdModal';
-import { Game } from './types';
+import Games from './views/Games';
 
 function App() {
-  const [games, setGames] = useState<Game[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data);
-      });
-  }, []);
-
   return (
-    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
-      <img src={LogoImage} alt="Logo NLW eSports" />
-
-      <h1 className='text-6xl text-white font-black mt-20'>
-        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
-      </h1>
-
-      <div className='grid grid-cols-6 gap-6 mt-16'>
-        {
-          games.map(game => {
-            return (
-              <GameBanner
-                key={game.id}
-                title={game.title}
-                bannerUrl={game.bannerUrl}
-                adsCount={game._count.ads}
-              />
-            )
-          })
-        }
-      </div>
-
-      <Dialog.Root>
-        <CreateAdBanner />
-
-        <CreateAdModal />
-      </Dialog.Root>
-    </div>
+    <Games />
   );
 };
 
