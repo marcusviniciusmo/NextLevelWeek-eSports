d8634bc61af81ec0ad2188537c662b326df1f32d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-13 17:06:03 +0100

    Fix game link tag (Web Project)

--- web/src/components/GameBanner/index.tsx
+++ web/src/components/GameBanner/index.tsx
@@ -1,18 +1,18 @@
 /* Types */
-import { GameBannerProps} from '../../types';
+import { GameBannerProps } from '../../types';
 /* Styles */
 import './styles.css';
 
 function GameBanner(props: GameBannerProps) {
   return (
-    <a href="#" id='gameBannerContainer'>
+    <div id='gameBannerContainer'>
       <img src={props.bannerUrl} alt={props.title} />
 
       <div>
         <strong>{props.title}</strong>
         <span>{props.adsCount} anúncio(s)</span>
       </div>
-    </a>
+    </div>
   );
 };
 
