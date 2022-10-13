c727329f4795aa1964dd4ebdaf87419ee0ebd33f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-10-13 10:00:03 +0100

    Code Review in the Input component (Web Project)

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,11 +1,13 @@
 /* Types */
 import { InputProps } from '../../types';
+/* Styles */
+import './styles.css';
 
 function Input(props: InputProps) {
   return (
     <input
       {...props}
-      className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+      className='inputComponent'
     />
   );
 };
